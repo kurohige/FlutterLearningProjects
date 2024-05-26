35c493ba9af34e0cc0250a1f711d753c36d7c035
Kurohige <jhidalgopalacios@yahoo.com>
2024-05-26 13:32:31 +0300

    changes to layout

---
+++ meals/lib/screens/categories.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:meals/data/dummy_data.dart';
+import 'package:meals/widgets/category_grid_item.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Pick your Categories'),
+      ),
+      body: GridView(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+        ),
+        children: availableCategories.map(
+          (category) {
+            return CategoryGridItem(category: category);
+          },
+        ).toList(),
+      ),
+    );
+  }
+}
